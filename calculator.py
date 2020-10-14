fd359d0cda9960e0d540d2fa3a636aa4e1462a9a
Yoni Hassine <hassineyoni@gmail.com>
2020-10-15 01:26:08 +0200

    New modifications

--- calculator.py
+++ calculator.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 15 00:54:50 2020
-
 @author: hassi
 """
-
 """
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
-
