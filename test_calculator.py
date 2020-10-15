323f0c97b4e6f55e8d87dfaeb70888fca560d433
Yoni Hassine <55541756+yonihassine@users.noreply.github.com>
2020-10-15 02:22:33 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -12,5 +12,4 @@ class TestCalculator:
         assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
-        
+        assert 2 == calculator.subtract(4, 2)   
