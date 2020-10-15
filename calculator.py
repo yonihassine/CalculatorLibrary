8727717f29f2ff54685b7935451caaf660b4280f
Yoni Hassine <55541756+yonihassine@users.noreply.github.com>
2020-10-15 21:43:26 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,8 +6,11 @@ Created on Thu Oct 15 00:54:50 2020
 """
 Calculator library containing basic math operations.
 """
+
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
