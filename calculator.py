416430b2c45c92758dbe0c6ea74a2e9fe782147d
Yoni Hassine <55541756+yonihassine@users.noreply.github.com>
2020-10-15 21:40:53 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,6 +11,3 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
-
-def multiply(first_term, second_term):
-    return first_term * second_term
